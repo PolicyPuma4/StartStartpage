9b460a80ddede568c9211e1f93edb61988303998
PolicyPuma4 <83885295+PolicyPuma4@users.noreply.github.com>
2022-12-18 01:28:28 +0000

    Update main.go

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"strings"
 )
 
+var ssp_prefs string = "53308301f70432e8195ac16843b38ea5c0ab0e03080d61edd06d725a311fc3c0597636e9064b5df843962b8fd795febd252657d9b0cd79587c886ba0e5e953e3254d1068bc2e010e1ce4d2293e"
+
 func root(w http.ResponseWriter, r *http.Request) {
 	query := strings.TrimSpace(r.URL.Query().Get("q"))
 	if query == "" {
-		http.Redirect(w, r, "https://startpage.com", http.StatusSeeOther)
+		http.Redirect(w, r, "https://startpage.com?prfe="+ssp_prefs, http.StatusSeeOther)
 		return
 	}
 
@@ -23,7 +25,8 @@ func root(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !matched {
-		http.Redirect(w, r, "https://startpage.com/do/metasearch.pl?query="+url.QueryEscape(query), http.StatusSeeOther)
+		// Open search in new tab disabled, safe search disabled and server region set to EU servers
+		http.Redirect(w, r, "https://www.startpage.com/sp/search?query="+url.QueryEscape(query)+"&prfe="+ssp_prefs, http.StatusSeeOther)
 		return
 	}
 
